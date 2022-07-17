cf8b94f81cb3a653e9f5c784f3ac5e08b752ee39
robson3222 <robson3222@gmail.com>
2022-07-17 17:41:38 -0300

    add @PUT

--- src/carros/carros.controller.ts
+++ src/carros/carros.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { CarroService } from './shared/carro.service';
 import { Carro } from './shared/carro';
 
@@ -9,21 +9,31 @@ export class CarrosController {
         private carroService: CarroService
     ) { }
 
-        @Get()
-        async getAll() : Promise<Carro[]> {
-            return this.carroService.getAll();
-        }
+    @Get()
+    async getAll(): Promise<Carro[]> {
+        return this.carroService.getAll();
+    }
 
-        @Get(':id')
-        async getById(@Param('id') id: number) : Promise<Carro> {
-            return this.carroService.getById(id);
-        }
+    @Get(':id')
+    async getById(@Param('id') id: number): Promise<Carro> {
+        return this.carroService.getById(id);
+    }
 
-       @Post()
-       async create(@Body() carro: Carro): Promise<Carro> {
+    @Post()
+    async create(@Body() carro: Carro): Promise<Carro> {
         return this.carroService.create(carro);
-       }
-
+    }
+    @Put('id')
+    async update(@Param(id) id: number, @Body() carro:Carro): Promise<Carro> {
+        carro.id = id;
+        return this.CarroService.update(carro);
         
-    
+    }
+    @Delete(':id')
+    async delete(@Param('id') id: number) {
+        this.carroService.delete(id);
+    }
+
+
+
 }
